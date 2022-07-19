b6835976f1d79ea45ef4e2c1174c3fdc6308c097
Daniel Hernandez Torres <daedheto@gmail.com>
2022-07-18 21:13:59 -0300

    correcciónDesafio

--- src/main.js
+++ src/main.js
@@ -9,13 +9,29 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  for (let i = 0; i < productos.length; i++) {
+    const idProducto = productos[i];
+    console.log(idProducto);
+  }
   res.render("inicio", { productos });
 });
 
 app.post("/productos", (req, res) => {
-  productos.push(req.body);
+  const cargaProductos = productos.push(req.body);
   console.log(productos);
+  console.log("id", cargaProductos);
   res.redirect("/");
+});
+
+app.put("/api/productos/:id", (req, res) => {
+  const actualizarProducto = productos.find(req.params.id);
+  actualizarProducto.push;
+  res.json(req.body);
+});
+
+app.delete("/api/productos/:id", (req, res) => {
+  const borrarProducto = productos.find(req.params.id);
+  res.json(borrarProducto([]));
 });
 
 const PORT = 8080;
